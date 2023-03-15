caffb0d28230bad253926387490a8a8289788a69
Josh <joshua.winters@tsymmetry.com>
2023-03-15 19:35:03 -0400

    a simple bandname generator, only 1 commit, as its quick and simple

---
+++ python-bandname-generator.py
@@ -0,0 +1,6 @@
+print("Greetings, and welcome to the band name generator!")
+name = input("Hello, please tell me your name:\n")
+city = input("And what city are you from?\n")
+pet = input("finally, what is your pet's name?\n")
+
+print("Your band name could be: " + city + " " + pet + " " + name)
